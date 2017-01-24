43862e61f681dfb96ea332841fce11b618b5f9b0
Shanmugam MPl <git@shan.mp>
2017-01-24 18:09:52 -0500

    Ball rolls around!

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
+	public float speed;
+	private Rigidbody rb;
 
-	// Use this for initialization
-	void Start () {
-		
+	void Start(){
+		rb = GetComponent<Rigidbody> ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void FixedUpdate(){
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
+
+		rb.AddForce (movement*speed);
+
 	}
 }
