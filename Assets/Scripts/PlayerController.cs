63b45439775760c749c9e0f636ad1cbdd75fa47b
Shanmugam MPl <git@shan.mp>
2017-01-26 15:45:36 -0500

    Add count and Game Over Text

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,14 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 	public float speed;
+	public Text countText;
+	public Text gameoverText;
 	private Rigidbody rb;
 	private int count;
 
 	void Start(){
 		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		SetCountText ();
+		gameoverText.text = "";
 	}
 
 	void FixedUpdate(){
@@ -26,6 +32,14 @@ public class PlayerController : MonoBehaviour {
 		if(other.gameObject.CompareTag("Pick Up")){
 			other.gameObject.SetActive(false);
 			count++;
+			SetCountText ();
 		}	
+	}
+	void SetCountText(){
+		countText.text = "Count: " + count.ToString ();
+		if (count>=12)
+		{
+			gameoverText.text = "YOU WIN!";
+		}
 	}
 }
